ee4b3a91342156c51eece65c46107e53d02444c9
rnandory <tntjs2637@gmail.com>
2024-09-24 18:43:19 +0900

    ajax 시작

--- cohort8th/src/main/java/com/newlecture/web/cohort8th/controller/MenuController.java
+++ cohort8th/src/main/java/com/newlecture/web/cohort8th/controller/MenuController.java
@@ -24,6 +24,40 @@ public class MenuController {
     private CategoryService categoryService;
 
 
+    /*
+    method  url                     payload
+    GET     api/menus?c=1&q=ame         x
+    GET     api/menu/3                  x
+    POST    api/menus               menu{}
+    PUT     api/menu/3              menu{}
+    DELETE  api/menu/3              [menu{}]
+
+    url: page명처럼 행위가 아니라 what(어떤 data?)
+    => Rest방식으로 명명. like 주소체계.
+        상위 ... 하위포함관계 순서 지킬 것!
+        행위명을 쓰면 안된다! 행위는 이미 method에 묻어있다
+
+    RESTful API? vs REST API
+    행위명을 가급적 자제하겠지만, 필요에 의해서 행위명을 쓸 수도 있다
+     */
+    @GetMapping("api/menus")
+    public List<MenuView> getList(
+            Model model,
+            @RequestParam(name = "c", required = false)
+            Integer categoryId,
+
+            @RequestParam(name = "q", required = false)
+            String query,
+
+            @RequestParam(name = "order", required = false)
+            String order
+    ) {
+
+        List<MenuView> menus = service.getList(categoryId, query, order);
+
+        return menus;
+    }
+
     @GetMapping("list")
     public String list(
             Model model,
